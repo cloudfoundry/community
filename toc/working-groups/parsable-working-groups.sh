b78ff3f22e3f118b6f46e02f65ca138e1a98b3c2
Beyhan Veli <beyhan.veli@sap.com>
2025-02-12 13:48:18 +0100

    Ignore concourse WG in parsable-working-groups

--- toc/working-groups/parsable-working-groups.sh
+++ toc/working-groups/parsable-working-groups.sh
@@ -11,7 +11,7 @@ for group in $(ls ../*.md | grep -v -e ROLES -e CHANGEPLAN -e PRINCIPLES -e GOVE
         ruby -rjson -ryaml -e "puts YAML.load(ARGF.read).to_json" 2> /dev/null | jq '[.]'
 done
 
-for working_group in $(ls *.md | grep -v -e WORKING-GROUPS -e paketo -e vulnerability); do
+for working_group in $(ls *.md | grep -v -e WORKING-GROUPS -e paketo -e vulnerability -e concourse); do
       cat ${working_group} | awk '/^```yaml$/{flag=1;next}/^```$/{flag=0}flag' | \
             ruby -rjson -ryaml -e "puts YAML.load(ARGF.read).to_json" 2> /dev/null | jq '[.]'
 done
