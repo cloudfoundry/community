33637771cedb89fdee7f94126a09fbcfe6e60f3d
Pascal Zimmermann <pascal.zimmermann@theiotstudio.com>
2025-06-25 07:50:52 +0200

    feat: Remove jq

--- toc/working-groups/contributions-for-user.sh
+++ toc/working-groups/contributions-for-user.sh
@@ -15,7 +15,7 @@ TOC_JSON=$(${repo_root}/toc/working-groups/parsable-working-groups.sh)
 
 declare -a WORKING_GROUPS
 IFS=$'\n'
-WORKING_GROUPS=($(echo "${TOC_JSON}" | jq -r '.[].name'))
+WORKING_GROUPS=($(echo "${TOC_JSON}" | yq -oj -I=0 -r '.[].name'))
 
 list_working_groups() {
   for wg in "${WORKING_GROUPS[@]}"; do
@@ -34,14 +34,19 @@ usage_checks() {
     exit 1
   fi
 
-  if [[ ! -e $(which jq) ]]; then
-      echo 'The `jq` cli (https://github.com/jqlang/jq) is required for this script' >&2
-      exit 1
+  # Extract version, remove the leading 'v' if present and check the installed version
+  installed_yq_version=$(yq --version 2>&1 | awk '{print $NF}' | sed 's/^v//')
+  # yq v4.7 is required for the script, though the `@uri` feature is available from v4.31.1 onwards
+  required_version="4.31.1"
+
+  if [ "$(printf '%s\n' "${required_version}" "${installed_yq_version}" | sort -V | head -n1)" != "$required_version" ]; then
+    echo "yq version ${installed_yq_version} is installed, but version ${required_version} or higher is required." >&2
+    exit 1
   fi
 
   if gist_url=$(echo "test" | gh gist create -d test -f test - 2>/dev/null); then
     gh gist delete "${gist_url}" --yes >/dev/null 2>&1
-  else 
+  else
     echo 'Please login to GitHub with the `gh` cli using credentials with permissions to create gists' >&2
     exit 1
   fi
@@ -71,63 +76,109 @@ find_commits_for_repo() {
   local repo=$2
 
   if commits=$(gh api --paginate "repos/${repo}/commits?author=${user}&per_page=100" 2>/dev/null); then
-    echo "${commits}" |  jq -r '.[] | "- " + .commit.author.date + ": [" + (.commit.message | split("\n"))[0] + "](" + .html_url + ")"'
+    echo "${commits}" | interactions=$(yq -oj -I=0 -r '
+      .[] |
+      "- " + .commit.author.date + ": [" + (.commit.message | split("\n")[0]) + "](" + .html_url + ")"
+    ') yq -oj -r '(select(strenv(interactions) == "- ") | . = "") // strenv(interactions)'
   fi
   if commits=$(gh api --paginate "repos/${repo}/commits?committer=${user}&per_page=100" 2>/dev/null); then
-    echo "${commits}" |  jq -r '.[] | "- " + .commit.committer.date + ": [" + (.commit.message | split("\n"))[0] + "](" + .html_url + ")"'
+    echo "${commits}" | interactions=$(yq -oj -I=0 -r '
+      .[] |
+      "- " + .commit.committer.date + ": [" + (.commit.message | split("\n")[0]) + "](" + .html_url + ")"
+    ') yq -oj -r '(select(strenv(interactions) == "- ") | . = "") // strenv(interactions)'
   fi
 }
 
 find_prs_for_repo() {
   local user=$1
   local repo=$2
-  
-  echo "${EVENTS_JSON}" | jq -r --arg repo "${repo}" '.[] | select([.type] | inside(["PullRequestReviewEvent"])) | select(.repo.name == $repo) | "- " + .payload.pull_request.created_at + ": [" +.payload.pull_request.title + "](" + .payload.pull_request.html_url + ")"'
-  echo "${EVENTS_JSON}" | jq -r --arg user "${user}" --arg repo "${repo}" '.[] | select([.type] | inside(["PullRequestReviewCommentEvent"])) | select(.repo.name == $repo) | select(.payload.pull_request.user.login != $user) | "- " + .payload.pull_request.created_at + ": [" +.payload.pull_request.title + "](" + .payload.pull_request.html_url + ")"'
-  
+
+  echo "${EVENTS_JSON}" | interactions=$(repository="${repo}" yq -oj -I=0 -r '
+    .[]
+    | select(.type == "PullRequestReviewEvent")
+    | select(.repo.name == strenv(repository))
+    | "- " + .payload.pull_request.created_at + ": [" + .payload.pull_request.title + "](" + .payload.pull_request.html_url + ")"
+  ') yq -oj -r '(select(strenv(interactions) == "- ") | . = "") // strenv(interactions)'
+
+  echo "${EVENTS_JSON}" | interactions=$(user="${user}" repository="${repo}" yq -oj -I=0 -r '
+    .[]
+    | select(.type == "PullRequestReviewCommentEvent")
+    | select(.repo.name == strenv(repository))
+    | select(.payload.pull_request.user.login != strenv(user))
+    | "- " + .payload.pull_request.created_at + ": [" + .payload.pull_request.title + "](" + .payload.pull_request.html_url + ")"
+  ') yq -oj -r '(select(strenv(interactions) == "- ") | . = "") // strenv(interactions)'
 }
 
 find_issues_for_repo() {
   local user=$1
   local repo=$2
-  
+
   if issues=$(gh api --paginate "repos/${repo}/issues?creator=${user}&state=all" 2>/dev/null); then
-    echo "${issues}" | jq -r '.[] | "- " + .created_at + ": [" + .title + "](" + .html_url + ")"'
+    echo "${issues}" | interactions=$(yq -oj -I=0 -r '
+      .[]
+      | "- " + .created_at + ": [" + .title + "](" + .html_url + ")"
+    ') yq -oj -r '(select(strenv(interactions) == "- ") | . = "") // strenv(interactions)'
   fi
   if issues=$(gh api --paginate "repos/${repo}/issues?mentioned=${user}&state=all" 2>/dev/null); then
-    echo "${issues}" | jq -r '.[] | "- " + .created_at + ": [" + .title + "](" + .html_url + ")"'
+    echo "${issues}" | interactions=$(yq -oj -I=0 -r '
+      .[]
+      | "- " + .created_at + ": [" + .title + "](" + .html_url + ")"
+    ') yq -oj -r '(select(strenv(interactions) == "- ") | . = "") // strenv(interactions)'
   fi
-  echo "${EVENTS_JSON}" | jq -r --arg user "${user}" --arg repo "${repo}" '.[] | select([.type] | inside(["IssueCommentEvent"])) | select(.repo.name == $repo) | select(.payload.issue.user.login != $user) | "- " + .payload.issue.created_at + ": [" +.payload.issue.title + "](" + .payload.issue.html_url + ")"'
+
+  echo "${EVENTS_JSON}" |  interactions=$(user="${user}" repo="${repo}" yq -oj -I=0 -r '
+    .[]
+    | select(.type == "IssueCommentEvent")
+    | select(.repo.name == strenv(repo))
+    | select(.payload.issue.user.login != strenv(user))
+    | "- " + .payload.issue.created_at + ": [" + .payload.issue.title + "](" + .payload.issue.html_url + ")"
+  ') yq -oj -r '(select(strenv(interactions) == "- ") | . = "") // strenv(interactions)'
 }
 
-  usage_checks
-  set -u
+usage_checks
+set -u
 
-  echo "Gathering GH events..."
-  EVENTS_JSON=$(gh api --paginate "users/${USERNAME}/events?per_page=100")
+echo "Gathering GH events..."
+EVENTS_JSON=$(gh api --paginate "users/${USERNAME}/events?per_page=100")
 
-  declare -a areas
-  areas=($(echo "${TOC_JSON}" | jq --arg wg_name "${WORKING_GROUP}" -r '.[] | select(.name == $wg_name) | .areas[].name'))
-  for area in "${areas[@]}"; do
-    echo "Gathering contributions for the '${WORKING_GROUP}: ${area}' area..."
-    uri_safe_file=$(echo "${wg} - ${area}.md" | jq  -sRr '@uri' | sed s/%20/\ /g | sed s/%0A//g | sed s/%2F/and/g)
-    gist_url=$( (
-    echo "# ${WORKING_GROUP}: ${area} Contributions"
-    declare -a repos
-    repos=($(echo "${TOC_JSON}" | jq -r --arg wg_name "${WORKING_GROUP}" --arg area "${area}" '.[] | select(.name == $wg_name) | .areas[] | select(.name == $area) | .repositories[]'))
-    echo "### PRs Commented on/Reviewed:"
-    for repo in "${repos[@]}"; do
-      find_prs_for_repo "${USERNAME}" "${repo}"
-    done | sort -u
-    echo "### Issues that may be relevant:"
-    for repo in "${repos[@]}"; do
-      find_issues_for_repo  "${USERNAME}" "${repo}"
-    done | sort -u
-    echo "### Code contributions:"
-    for repo in "${repos[@]}"; do
-        find_commits_for_repo "${USERNAME}" "${repo}" 
-    done | sort -u
-    echo
-    ) | gh gist create -d "${USERNAME}'s Possible Contributions to ${WORKING_GROUP} - ${area}" -f "${uri_safe_file}" - 2>/dev/null)
-    echo "Contributions have been summarized at ${gist_url}"
-  done
+declare -a areas
+areas=($(echo "${TOC_JSON}" | wg_name="${WORKING_GROUP}" yq -oj -r -I=0 -r "
+  .[]
+  | select(.name == strenv(wg_name))
+  | .areas[].name
+"))
+
+for area in "${areas[@]}"; do
+  echo "Gathering contributions for the '${WORKING_GROUP}: ${area}' area..."
+  uri_safe_file=$(echo "${wg} - ${area}.md" | yq -oj -I=0 -r '@uri' | sed -E 's/\%20|\+|\%28/\ /g' | sed -E 's/\%0A|\%29//g' | sed 's/%2F/and/g')
+  gist_url=$(
+    (
+      echo "# ${WORKING_GROUP}: ${area} Contributions"
+      declare -a repos
+      repos=($(echo "${TOC_JSON}" | wg_name="${WORKING_GROUP}" area="${area}" yq -oj -I=0 -r '
+        .[]
+        | select(.name == strenv(wg_name))
+        | .areas[]
+        | select(.name == strenv(area))
+        | .repositories[]
+      '))
+
+      echo "### PRs Commented on/Reviewed:"
+      for repo in "${repos[@]}"; do
+        find_prs_for_repo "${USERNAME}" "${repo}"
+      done | sort -u
+
+      echo "### Issues that may be relevant:"
+      for repo in "${repos[@]}"; do
+        find_issues_for_repo  "${USERNAME}" "${repo}"
+      done | sort -u
+
+      echo "### Code contributions:"
+      for repo in "${repos[@]}"; do
+        find_commits_for_repo "${USERNAME}" "${repo}"
+      done | sort -u
+      echo
+    ) | gh gist create -d "${USERNAME}'s Possible Contributions to ${WORKING_GROUP} - ${area}" -f "${uri_safe_file}" - 2>/dev/null
+  )
+  echo "Contributions have been summarized at ${gist_url}"
+done
--- toc/working-groups/parsable-working-groups.sh
+++ toc/working-groups/parsable-working-groups.sh
@@ -4,7 +4,7 @@ set -eu -o pipefail
 
 get_working_group_info_json() {
   local group=$1
-  echo $(cat ${group} | awk '/^```yaml$/{flag=1;next}/^```$/{flag=0}flag' | yq -o=json '.')
+  echo $(cat ${group} | awk '/^```yaml$/{flag=1;next}/^```$/{flag=0}flag' | yq -oj '.')
 }
 
 repo_root=$(dirname $(cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd))
@@ -14,11 +14,11 @@ pushd $repo_root/toc/working-groups >/dev/null
 working_group_info="[]"
 
 for group in $(ls ../*.md | grep -v -e ROLES -e CHANGEPLAN -e PRINCIPLES -e GOVERNANCE); do
-     working_group_info=$(echo "${working_group_info}" | jq --argjson value "$(get_working_group_info_json $group)" '. += [ $value ]')
+     working_group_info=$(echo "${working_group_info}" | working_group_information_json="$(get_working_group_info_json $group)" yq -oj -I=0 '. + [env(working_group_information_json)]')
 done
 
 for working_group in $(ls *.md | grep -v -e WORKING-GROUPS -e paketo -e vulnerability -e concourse); do
-     working_group_info=$(echo "${working_group_info}" | jq --argjson value "$(get_working_group_info_json $working_group)" '. += [ $value ]')
+     working_group_info=$(echo "${working_group_info}" | working_group_information_json="$(get_working_group_info_json $working_group)" yq -oj -I=0 '. + [env(working_group_information_json)]')
 done
 
 echo $working_group_info
